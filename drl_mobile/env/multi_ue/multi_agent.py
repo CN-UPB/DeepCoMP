93a021a6c0c88873e71df24a8d582b6132c87af4
Stefan Schneider <stefan.schneider@upb.de>
2020-06-29 17:20:31 +0200

    implemented step for multi-agent env

--- drl_mobile/env/multi_ue/multi_agent.py
+++ drl_mobile/env/multi_ue/multi_agent.py
@@ -1,6 +1,6 @@
+import numpy as np
 from ray.rllib.env.multi_agent_env import MultiAgentEnv
 
-from drl_mobile.env.single_ue.base import MobileEnv
 from drl_mobile.env.single_ue.variants import DatarateMobileEnv
 
 
@@ -33,4 +33,39 @@ class MultiAgentMobileEnv(MultiAgentEnv, DatarateMobileEnv):
         :param action_dict: Dict of UE IDs --> selected action
         :return: obs, rewards, dones, infos. Again in the form of dicts: UE ID --> value
         """
-        raise NotImplementedError('TODO')
+        # TODO: avoid duplicate code; write function for joint parts of step in base, central, and multi-agent
+        prev_obs = self.obs
+        obs = {}
+        rewards = {}
+
+        # step all UEs
+        for ue in self.ue_list:
+            penalty = 0
+
+            # apply action for each UE; 0= noop
+            if action_dict[ue.id] > 0:
+                bs = self.bs_list[action_dict[ue.id] - 1]
+                # penalty of -3 for unsuccessful connection attempt
+                penalty -= 3 * (not ue.connect_to_bs(bs, disconnect=True))
+
+            # move and calc reward for UE
+            reward_before = self.calc_reward(ue, penalty)
+            num_lost_conn = ue.move()
+            # add penalty of -1 for each lost connection through movement (rather than actively disconnected)
+            penalty -= num_lost_conn
+            reward_after = self.calc_reward(ue, penalty)
+            rewards[ue.id] = np.mean([reward_before, reward_after])
+
+            # next obs
+            obs[ue.id] = self.get_obs(ue)
+
+        self.time += 1
+
+        # return next obs, reward, done, infos (after incrementing time!)
+        self.obs = obs
+        # done and info are the same for all UEs
+        done = self.time >= self.episode_length
+        dones = {ue.id: done for ue in self.ue_list}
+        infos = {ue.id: self.time for ue in self.ue_list}
+        self.log.info("Step", time=self.time, prev_obs=prev_obs, action=action_dict, rewards=rewards, next_obs=self.obs, done=done)
+        return self.obs, rewards, dones, infos
