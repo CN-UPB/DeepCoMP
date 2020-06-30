cedf716747e45452b8a27c4e802e7b265f2b726d
Stefan Schneider <stefan.schneider@upb.de>
2020-06-30 16:19:25 +0200

    trying to debug structlog

--- debug.py
+++ debug.py
@@ -5,10 +5,10 @@ import structlog
 from shapely.geometry import Point
 import ray.rllib.agents.ppo as ppo
 
-from drl_mobile.env.map import Map
-from drl_mobile.env.user import User
-from drl_mobile.env.station import Basestation
-from drl_mobile.env.env import DatarateMobileEnv
+# from drl_mobile.env.map import Map
+# from drl_mobile.env.user import User
+# from drl_mobile.env.station import Basestation
+# from drl_mobile.env.env import DatarateMobileEnv
 
 
 class Test:
@@ -20,11 +20,11 @@ class Test:
 test = Test(1)
 
 # for real env
-map = Map(width=150, height=100)
-ue1 = User('ue1', map, color='blue', pos_x='random', pos_y=40, move_x='slow')
-# ue2 = User('ue2', color='red', pos_x='random', pos_y=30, move_x='fast')
-bs1 = Basestation('bs1', pos=Point(50, 50))
-bs2 = Basestation('bs2', pos=Point(100, 50))
+# map = Map(width=150, height=100)
+# ue1 = User('ue1', map, color='blue', pos_x='random', pos_y=40, move_x='slow')
+# # ue2 = User('ue2', color='red', pos_x='random', pos_y=30, move_x='fast')
+# bs1 = Basestation('bs1', pos=Point(50, 50))
+# bs2 = Basestation('bs2', pos=Point(100, 50))
 
 # create env_config for RLlib instead
 # env_config = {
@@ -44,7 +44,7 @@ bs2 = Basestation('bs2', pos=Point(100, 50))
 # config['env_config'] = env_config
 
 # does this work?
-copy_conf = deepcopy(ue1)
+copy_conf = deepcopy(test)
 print(copy_conf)
 
 # still works
