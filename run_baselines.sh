4fb4c8c1243c6cb6099cf97ac63d127b3d643f49
Stefan Schneider <stefan.schneider@upb.de>
2020-07-14 14:57:53 +0200

    script for running baselines

---
+++ run_baselines.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# script for running multiple experiments sequentially
+
+for num_ues in {1..10}
+do
+  echo Num. UEs: $num_ues
+  deepcomp --workers 15 --eps-length 100 --alg random --agent multi --env medium --slow-ues $num_ues --eval 50
+  deepcomp --workers 15 --eps-length 100 --alg greedy-best --agent multi --env medium --slow-ues $num_ues --eval 50
+  deepcomp --workers 15 --eps-length 100 --alg greedy-all --agent multi --env medium --slow-ues $num_ues --eval 50
+done
