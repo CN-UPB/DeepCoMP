46b0a7c9fb11867e093465cff6d8f64590a848ae
Stefan Schneider <stefan.schneider@upb.de>
2020-07-14 15:29:22 +0200

    make num ues in run script configurable

--- run_baselines.sh
+++ run_baselines.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # script for running multiple experiments sequentially
-# pass num workers as 1st cli arg
+# CLI args: num_workers, min_ues, max_ues
 
-for num_ues in {1..2}
+for num_ues in {$2..$3}
 do
   echo Num. UEs: $num_ues
   deepcomp --workers $1 --eps-length 100 --alg random --agent central --env medium --slow-ues $num_ues --eval 50
