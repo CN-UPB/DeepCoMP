2643484030d78d4fc25c4feef08f4aadfc9cc0a2
Stefan Schneider <stefan.schneider@upb.de>
2020-07-14 16:14:33 +0200

    plot reward over incr num UE

--- drl_mobile/util/eval.py
+++ drl_mobile/util/eval.py
@@ -1,10 +1,12 @@
 """Useful scripts for evaluation"""
 import os
 from ast import literal_eval
+from collections import defaultdict
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 from drl_mobile.util.constants import TRAIN_DIR, EVAL_DIR, PLOT_DIR
 
@@ -88,14 +90,74 @@ def plot_ppo_mean_eps_reward(df):
     return int(eps_per_iter)
 
 
+# summarizing results from different runs
+def summarize_results(dir=EVAL_DIR):
+    """Read and summarize all results in a directory. Return a df."""
+    config_cols = ['alg', 'agent', 'num_ue_slow', 'num_ue_fast', 'eps_length', 'env_size']
+    files = os.listdir(dir)
+    data = defaultdict(list)
+
+    # read all files and save relevant data
+    for f in files:
+        df = pd.read_csv(f'{dir}/{f}')
+        # copy config
+        for conf in config_cols:
+            data[conf].append(df[conf][0])
+        # summarize num ues
+        data['num_ue'].append(df['num_ue_slow'][0] + df['num_ue_fast'][0])
+        # summarize eps reward
+        data['num_eps'].append(len(df['eps_reward']))
+        data['eps_reward_mean'].append(np.mean(df['eps_reward']))
+        data['eps_reward_std'].append(np.std(df['eps_reward']))
+
+    # create and return combined df
+    return pd.DataFrame(data=data)
+
+
+def concat_results(dir=EVAL_DIR):
+    """Read results and concat df for plotting with seaborn"""
+    dfs = []
+    for f in os.listdir(dir):
+        dfs.append(pd.read_csv(f'{dir}/{f}'))
+    return pd.concat(dfs)
+
+
+def plot_increasing_ues(df):
+    """Plot results for increasing num. UEs. Takes summarized df as input."""
+    for alg in df['alg'].unique():
+        df_alg = df[df['alg'] == alg]
+        plt.errorbar(df_alg['num_ue'], df_alg['eps_reward_mean'], yerr=df_alg['eps_reward_std'], capsize=5, label=alg)
+
+    # axes and legend
+    plt.xlabel("Num. UEs")
+    plt.ylabel("Episode Reward")
+
+    # remove error bars from legend: https://stackoverflow.com/a/15551976/2745116
+    # get handles
+    # handles, labels = plt.get_legend_handles_labels()
+    # # remove the errorbars
+    # handles = [h[0] for h in handles]
+    # # use them in the legend
+    # plt.legend(handles, labels, numpoints=1)
+
+    plt.legend()
+
+    plt.show()
+
+
 if __name__ == '__main__':
-    df_ppo_org, df_ppo = read_training_progress('PPO_MultiAgentMobileEnv_0_2020-07-14_09-34-32asp5wtp5')
-    df_greedy_best = read_testing_results('GreedyBestSelection_MultiAgentMobileEnv_2020-07-14_10-52-16.csv')
-    df_greedy_all = read_testing_results('GreedyAllSelection_MultiAgentMobileEnv_2020-07-14_10-56-44.csv')
-    df_rand = read_testing_results('RandomAgent_CentralMultiUserEnv_2020-07-14_11-07-26.csv')
+    # df_ppo_org, df_ppo = read_training_progress('PPO_MultiAgentMobileEnv_0_2020-07-14_09-34-32asp5wtp5')
+    # df_greedy_best = read_testing_results('GreedyBestSelection_MultiAgentMobileEnv_2020-07-14_10-52-16.csv')
+    # df_greedy_all = read_testing_results('GreedyAllSelection_MultiAgentMobileEnv_2020-07-14_10-56-44.csv')
+    # df_rand = read_testing_results('RandomAgent_CentralMultiUserEnv_2020-07-14_11-07-26.csv')
+    #
+    # dfs = [df_greedy_best, df_greedy_all, df_rand]
+    # labels = ['Greedy-Best', 'Greedy-All', 'Random']
+    #
+    # eps_per_iter = plot_ppo_mean_eps_reward(df_ppo_org)
+    # plot_eps_reward(dfs, labels, roll_mean_window=eps_per_iter, filename='eps_reward.pdf')
 
-    dfs = [df_greedy_best, df_greedy_all, df_rand]
-    labels = ['Greedy-Best', 'Greedy-All', 'Random']
+    df = summarize_results()
+    # df = concat_results()
+    plot_increasing_ues(df)
 
-    eps_per_iter = plot_ppo_mean_eps_reward(df_ppo_org)
-    plot_eps_reward(dfs, labels, roll_mean_window=eps_per_iter, filename='eps_reward.pdf')
