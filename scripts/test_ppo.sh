9ded7a76ee0d5a8f4fcd9a4bf25bae0746f14e17
Stefan Schneider <stefan.schneider@upb.de>
2020-07-15 09:13:08 +0200

    test each rl agent with fixed num ues

--- scripts/test_ppo.sh
+++ scripts/test_ppo.sh
@@ -6,11 +6,11 @@
 # TODO: since each PPO test cannot be parallelized, I might run multiple tests in parallel using GNU parallel
 #  not sure if I can run multiple ray sessions in parallel? may break
 
+# load and test saved PPO agents in order; assuming they were for 1-10 agents respectively
+num_ues=1
 for dir in "$@"
 do
-  for num_ues in {1..1}
-  do
-    echo Dir: "$dir", Num. UEs: $num_ues
-    deepcomp --eps-length 100 --alg ppo --agent central --env medium --slow-ues $num_ues --eval 50 --test "$dir"
-  done
+  echo Dir: "$dir", Num. UEs: $num_ues
+  deepcomp --eps-length 100 --alg ppo --agent central --env medium --slow-ues $num_ues --eval 50 --test "$dir"
+  num_ues=num_ues + 1
 done
