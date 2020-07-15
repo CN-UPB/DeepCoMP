ae15a7eada0ee5192a2c7c47d3daa30f07115a25
Stefan Schneider <stefan.schneider@upb.de>
2020-07-15 09:18:11 +0200

    fixed test script

--- scripts/test_ppo.sh
+++ scripts/test_ppo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # script for loading and testing multiple trained PPO agents sequentially
-# CLI args: list of PPO result dirs to test (variable length)
+# CLI args: list of PPO result dirs to test (variable length); eg, './test_ppo.sh ../results/PPO/PPO*'
 # eg, "PPO_CentralMultiUserEnv_0_2020-07-14_17-30-04ckm_wang PPO_CentralMultiUserEnv_0_2020-07-14_17-43-46milkk4nd"
 
 # TODO: since each PPO test cannot be parallelized, I might run multiple tests in parallel using GNU parallel
@@ -12,5 +12,5 @@ for dir in "$@"
 do
   echo Dir: "$dir", Num. UEs: $num_ues
   deepcomp --eps-length 100 --alg ppo --agent central --env medium --slow-ues $num_ues --eval 50 --test "$dir"
-  num_ues=num_ues + 1
+  num_ues=$((num_ues + 1))
 done
