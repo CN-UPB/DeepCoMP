07edae3feab8ec9df22a681b1de5d195dccf2f69
Stefan Schneider <stefan.schneider@upb.de>
2020-07-23 08:35:02 +0200

    script for testing ppo with many UEs

--- scripts/test_ppo.sh
+++ scripts/test_ppo.sh
@@ -7,10 +7,15 @@
 #  not sure if I can run multiple ray sessions in parallel? may break
 
 # load and test saved PPO agents in order; assuming they were for 1-10 agents respectively
-num_ues=1
+min_ues=10
+max_ues=40
+step_ues=10
+
 for dir in "$@"
 do
-  echo Dir: "$dir", Num. UEs: $num_ues
-  deepcomp --eps-length 100 --alg ppo --agent central --env medium --slow-ues $num_ues --sharing proportional-fair --eval 50 --test "$dir"
-  num_ues=$((num_ues + 1))
+  for num_ues in $(seq $min_ues $step_ues $max_ues)
+  do
+    echo Dir: "$dir", Num. UEs: $num_ues, Min UEs: $min_ues, Max UEs: $max_ues, Step UEs: $step_ues
+    deepcomp --seed 42 --eps-length 100 --alg ppo --agent multi --env medium --slow-ues $num_ues --sharing proportional-fair --eval 100 --test "$dir"
+  done
 done
