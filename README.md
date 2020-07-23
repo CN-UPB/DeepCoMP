7b0f3dad58b373618d070d48bc98b8ca7fc550b4
Stefan Schneider <stefan.schneider@upb.de>
2020-07-23 08:22:52 +0200

    step ues in run script

--- README.md
+++ README.md
@@ -82,7 +82,7 @@ tango4, tango5, (swc01)
 * Evaluation: 
     * Double check all units in my scenario, esp. for movement, distance, dr. Makes sense?
         * Can I use a 5G use case with mm wave? Otherwise, remove 5G from the title
-    * Also compare multi-agent & centralized with limited training time
+    (* Also compare multi-agent & centralized with limited training time)
     * CDF of avg UE rate
     * Different utilities for each UE? Shift log function to cut x-axis at different points correspondign to the requirement
         * Then normalize data rates accordingly
--- scripts/run_baselines.sh
+++ scripts/run_baselines.sh
@@ -6,12 +6,13 @@
 num_workers=$1
 min_ues=$2
 max_ues=$3
-echo Num workers: $num_workers, Min UEs: $min_ues, Max UEs: $max_ues
+step_ues=$4
+echo Num workers: $num_workers, Min UEs: $min_ues, Max UEs: $max_ues, Step UEs: $step_ues
 
-for num_ues in $(seq $min_ues $max_ues)
+for num_ues in $(seq $min_ues $step_ues $max_ues)
 do
   echo Num. UEs: $num_ues
-  deepcomp --seed 42 --workers $num_workers --eps-length 100 --alg random --agent central --env medium --slow-ues $num_ues --sharing proportional-fair --eval 100
+#  deepcomp --seed 42 --workers $num_workers --eps-length 100 --alg random --agent central --env medium --slow-ues $num_ues --sharing proportional-fair --eval 100
   deepcomp --seed 42 --workers $num_workers --eps-length 100 --alg greedy-best --agent multi --env medium --slow-ues $num_ues --sharing proportional-fair --eval 100
   deepcomp --seed 42 --workers $num_workers --eps-length 100 --alg greedy-all --agent multi --env medium --slow-ues $num_ues --sharing proportional-fair --eval 100
 done
