55dac4d9c484b781b06eff27abac5e13f240e3ba
Stefan Schneider <stefan.schneider@upb.de>
2020-10-08 13:51:30 +0200

    undo
    Signed-off-by: Stefan Schneider <stefan.schneider@upb.de>

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup, find_packages
 # FIXME: structlog 20.1.0 doesn't support deepcopy
 requirements = [
     'ray[rllib]>=1.0',
-    # 'structlog>=20.1.0',
-    'git+https://github.com/stefanbschneider/structlog.git@dev',
+    'structlog>=20.1.0',
+    # 'git+https://github.com/stefanbschneider/structlog.git@dev',
     'structlog-round>=1.0',
     'shapely==1.7.0',
     'matplotlib==3.2.1',
