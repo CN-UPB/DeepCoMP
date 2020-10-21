c19e3d46e611b56132bf7dbabf6c9000cbdcebe7
Stefan Schneider <stefan.schneider@upb.de>
2020-10-21 08:59:28 +0200

    prepared script for running eval experiments with baselines
    Signed-off-by: Stefan Schneider <stefan.schneider@upb.de>

--- scripts/run_baselines.sh
+++ scripts/run_baselines.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 # script for running multiple experiments sequentially
-# CLI args: num_workers, min_ues, max_ues
 
-# TODO: use only 1 worker for reproducible results!
-num_workers=$1
-min_ues=$2
-max_ues=$3
-step_ues=$4
-echo Num workers: $num_workers, Min UEs: $min_ues, Max UEs: $max_ues, Step UEs: $step_ues
+min_ues=$1
+max_ues=$2
+step_ues=$3
+num_eval=1
+echo Min UEs: $min_ues, Max UEs: $max_ues, Step UEs: $step_ues, Num eval: $num_eval
 
+# IMPORTANT: use only 1 worker for reproducible results!
 for num_ues in $(seq $min_ues $step_ues $max_ues)
 do
   echo Num. UEs: $num_ues
-  deepcomp --seed 42 --workers $num_workers --eps-length 100 --alg random --agent central --env medium --slow-ues $num_ues --sharing resource-fair --eval 100
-  deepcomp --seed 42 --workers $num_workers --eps-length 100 --alg greedy-best --agent multi --env medium --slow-ues $num_ues --sharing resource-fair --eval 100
-  deepcomp --seed 42 --workers $num_workers --eps-length 100 --alg greedy-all --agent multi --env medium --slow-ues $num_ues --sharing resource-fair --eval 100
+  deepcomp --seed 42 --eps-length 100 --alg random --agent central --env medium --slow-ues $num_ues --sharing resource-fair --eval $num_eval
+  deepcomp --seed 42 --eps-length 100 --alg greedy-best --agent multi --env medium --slow-ues $num_ues --sharing resource-fair --eval $num_eval
+  deepcomp --seed 42 --eps-length 100 --alg greedy-all --agent multi --env medium --slow-ues $num_ues --sharing resource-fair --eval $num_eval
 done
