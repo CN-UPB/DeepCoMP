b474c9662b38e394d99b2fef1269ef731ba65b8b
Stefan Schneider <stefan.schneider@upb.de>
2020-10-29 16:27:29 +0100

    prepare scripts for 2x2 experiments
    Signed-off-by: Stefan Schneider <stefan.schneider@upb.de>

--- scripts/run_baselines_num-ue.sh
+++ scripts/run_baselines_num-ue.sh
@@ -12,7 +12,10 @@ echo Min UEs: $min_ues, Max UEs: $max_ues, Step UEs: $step_ues, Env: $env, Num e
 for num_ues in $(seq $min_ues $step_ues $max_ues)
 do
   echo Num. UEs: $num_ues
-  deepcomp --seed 42 --eps-length 10 --alg random --agent central --env $env --slow-ues $num_ues --eval $num_eval --video html
-  deepcomp --seed 42 --eps-length 10 --alg greedy-best --agent multi --env $env --slow-ues $num_ues --eval $num_eval --video html
-  deepcomp --seed 42 --eps-length 10 --alg greedy-all --agent multi --env $env --slow-ues $num_ues --eval $num_eval --video html
+#  deepcomp --seed 42 --alg random --agent central --env $env --slow-ues $num_ues --eval $num_eval --video html
+  deepcomp --seed 42 --alg greedy-best --agent multi --env $env --slow-ues $num_ues --eval $num_eval --video html
+  deepcomp --seed 42 --alg greedy-all --agent multi --env $env --slow-ues $num_ues --eval $num_eval --video html
+  # run brute force once optimizing sum utility and once optimizing min utility
+  deepcomp --seed 42 --alg brute-force --agent central --env $env --slow-ues $num_ues --eval $num_eval --video html --reward sum
+  deepcomp --seed 42 --alg brute-force --agent central --env $env --slow-ues $num_ues --eval $num_eval --video html --reward min
 done
--- scripts/train_ppo_num-ue.sh
+++ scripts/train_ppo_num-ue.sh
@@ -8,10 +8,11 @@ step_ues=$4
 env=$5
 agent=$6
 train_steps=$7
-echo Num workers: $num_workers, Min UEs: $min_ues, Max UEs: $max_ues, Step UEs: $step_ues, Env: $env, Agent: $agent, Train steps: $train_steps
+reward=$8
+echo Num workers: $num_workers, Min UEs: $min_ues, Max UEs: $max_ues, Step UEs: $step_ues, Env: $env, Agent: $agent, Train steps: $train_steps, Reward aggregation: $reward
 
 for num_ues in $(seq $min_ues $step_ues $max_ues)
 do
   echo Num. UEs: $num_ues
-  deepcomp --seed 42 --workers $num_workers --eps-length 100 --train-steps $train_steps --alg ppo --agent $agent --env $env --slow-ues $num_ues --eval 1 --video html
+  deepcomp --seed 42 --workers $num_workers --train-steps $train_steps --agent $agent --env $env --slow-ues $num_ues --eval 1 --video html --reward $reward
 done
