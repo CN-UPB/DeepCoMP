583a2cb3a85389cc691ecf0d802dcb1dbc80b412
Stefan Schneider <stefan.schneider@upb.de>
2020-11-05 13:47:11 +0100

    run script for copmaring baselines
    Signed-off-by: Stefan Schneider <stefan.schneider@upb.de>

--- scripts/run_baselines_num-ue.sh
+++ scripts/run_baselines_num-ue.sh
@@ -6,16 +6,17 @@ max_ues=$2
 step_ues=$3
 env=$4
 num_eval=1
-echo Min UEs: $min_ues, Max UEs: $max_ues, Step UEs: $step_ues, Env: $env, Num eval: $num_eval
+seed=1234
+echo Min UEs: $min_ues, Max UEs: $max_ues, Step UEs: $step_ues, Env: $env, Num eval: $num_eval, Seed: $seed
 
 # IMPORTANT: use only 1 worker for reproducible results!
 for num_ues in $(seq $min_ues $step_ues $max_ues)
 do
   echo Num. UEs: $num_ues
-#  deepcomp --seed 42 --alg random --agent central --env $env --slow-ues $num_ues --eval $num_eval --video html
-#  deepcomp --seed 42 --alg greedy-best --agent multi --env $env --slow-ues $num_ues --eval $num_eval --video html
-#  deepcomp --seed 42 --alg greedy-all --agent multi --env $env --slow-ues $num_ues --eval $num_eval --video html
+#  deepcomp --seed $seed --alg random --agent central --env $env --slow-ues $num_ues --eval $num_eval --video html
+  deepcomp --seed $seed --alg greedy-best --agent multi --env $env --slow-ues $num_ues --eval $num_eval --video html --new-ue-interval 20
+  deepcomp --seed $seed --alg greedy-all --agent multi --env $env --slow-ues $num_ues --eval $num_eval --video html --new-ue-interval 20
   # run brute force once optimizing sum utility and once optimizing min utility
-  deepcomp --seed 42 --alg brute-force --agent central --env $env --slow-ues $num_ues --eval $num_eval --video html --reward sum
-  deepcomp --seed 42 --alg brute-force --agent central --env $env --slow-ues $num_ues --eval $num_eval --video html --reward min
+  deepcomp --seed $seed --alg brute-force --agent central --env $env --slow-ues $num_ues --eval $num_eval --video html --new-ue-interval 20
+#  deepcomp --seed $seed --alg brute-force --agent central --env $env --slow-ues $num_ues --eval $num_eval --video html --reward min
 done
