7e7de42aa8b8ac13e1ebe9ade57ea6fbd9ec6100
Stefan Schneider <stefan.schneider@upb.de>
2020-11-10 14:39:37 +0100

    moved old rsults
    Signed-off-by: Stefan Schneider <stefan.schneider@upb.de>

--- scripts/test_ppo.sh
+++ scripts/test_ppo.sh
@@ -5,14 +5,15 @@
 
 # load and test saved PPO agents in order; using the num UEs specified below
 min_ues=1
-max_ues=5
+max_ues=1
 step_ues=1
+agent=central
 
 for dir in "$@"
 do
   for num_ues in $(seq $min_ues $step_ues $max_ues)
   do
-    echo Dir: "$dir", Num. UEs: $num_ues, Min UEs: $min_ues, Max UEs: $max_ues, Step UEs: $step_ues
-    deepcomp --seed 42 --eps-length 100 --alg ppo --agent multi --env medium --slow-ues $num_ues --sharing proportional-fair --eval 5 --test "$dir"
+    echo Dir: "$dir", Num. UEs: $num_ues, Min UEs: $min_ues, Max UEs: $max_ues, Step UEs: $step_ues, Agent: $agent
+    deepcomp --seed 1234 --agent $agent --env medium --slow-ues $num_ues --eval 1 --test "$dir" --video html --new-ue-interval 20
   done
 done
