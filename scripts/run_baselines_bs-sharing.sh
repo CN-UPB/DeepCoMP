44ecaf387656749c7fb39ba30ceca20897466a8b
Stefan Schneider <stefan.schneider@upb.de>
2020-11-11 09:54:08 +0100

    add workers to script
    Signed-off-by: Stefan Schneider <stefan.schneider@upb.de>

--- scripts/run_baselines_bs-sharing.sh
+++ scripts/run_baselines_bs-sharing.sh
@@ -2,8 +2,9 @@
 # script for running multiple experiments sequentially
 
 num_ues=$1
+workers=$2
 num_eval=1
-echo Num UEs: $num_ues, Num eval: $num_eval
+echo Num UEs: $num_ues, Num eval: $num_eval, Workers: $workers
 
 # IMPORTANT: use only 1 worker for reproducible results!
 for sharing in resource-fair rate-fair max-cap proportional-fair mixed
@@ -11,7 +12,7 @@ do
   for alg in brute-force
   do
     echo Sharing: $sharing, Alg: $alg
-    deepcomp --seed 42 --alg $alg --env medium --slow-ues $num_ues --eval $num_eval --video html --sharing $sharing
+    deepcomp --seed 42 --alg $alg --env medium --slow-ues $num_ues --eval $num_eval --video html --sharing $sharing --workers $workers
   done
 
 done
