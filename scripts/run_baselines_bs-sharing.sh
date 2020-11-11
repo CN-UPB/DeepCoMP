1897bfd26c817600773258415acfd24a81825a88
Stefan Schneider <stefan.schneider@upb.de>
2020-11-11 09:53:16 +0100

    adjust script to only run brute-force
    Signed-off-by: Stefan Schneider <stefan.schneider@upb.de>

--- scripts/run_baselines_bs-sharing.sh
+++ scripts/run_baselines_bs-sharing.sh
@@ -8,7 +8,7 @@ echo Num UEs: $num_ues, Num eval: $num_eval
 # IMPORTANT: use only 1 worker for reproducible results!
 for sharing in resource-fair rate-fair max-cap proportional-fair mixed
 do
-  for alg in greedy-best greedy-all brute-force
+  for alg in brute-force
   do
     echo Sharing: $sharing, Alg: $alg
     deepcomp --seed 42 --alg $alg --env medium --slow-ues $num_ues --eval $num_eval --video html --sharing $sharing
