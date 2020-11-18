2e2e42fca1a097df3317704527f9f16e24d6af49
Stefan Schneider <stefan.schneider@upb.de>
2020-11-18 14:57:42 +0100

    updated run script
    Signed-off-by: Stefan Schneider <stefan.schneider@upb.de>

--- scripts/run_baselines_bs-sharing.sh
+++ scripts/run_baselines_bs-sharing.sh
@@ -9,7 +9,7 @@ echo Num UEs: $num_ues, Num eval: $num_eval, Workers: $workers
 # IMPORTANT: use only 1 worker for reproducible results!
 for sharing in resource-fair rate-fair max-cap proportional-fair mixed
 do
-  for alg in brute-force
+  for alg in brute-force greedy-best greedy-all
   do
     echo Sharing: $sharing, Alg: $alg
     deepcomp --seed 42 --alg $alg --env medium --slow-ues $num_ues --eval $num_eval --video html --sharing $sharing --workers $workers
